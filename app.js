513338d81799360cbefb9f8f520835db9f9aed30
David Lundholm <david.lundholm@zocom.se>
2022-04-28 11:04:20 +0200

    Add environment validation and token test

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+
+const variables = ['JWT_SECRET', 'PORT']
+for(let variable of variables){
+  if(!process.env[variable]){
+    console.error("Missing environment variable " + variable)
+    process.exit(1)
+  }
+}
 
 const app = express()
 
+app.use( express.json() )
+
+app.use( (req,res,next) => {
+  if(process.env.NODE_ENV == 'development'){
+    const timeStamp = new Date()
+    console.log(timeStamp.toLocaleString(), req.method, req.path)
+  }
+  next()
+})
+
 app.get('/', (req,res) => {
   res.json({message: 'Hello World!'})
+})
+
+app.get('/hemlighet', (req,res) => {
+  if(!req.header('Authorization')){
+    res.status(403).json({error: 'Aja baja'})
+  }else{
+    const token = req.header('Authorization')
+    const data = jwt.verify(token, process.env.JWT_SECRET)
+    res.json(data)
+  }
+})
+
+app.post('/login', (req,res) => {
+  if(req.body.hemlighet === 'grillkorv'){
+    const token = jwt.sign(
+      {food:'bananpaj'},
+      process.env.JWT_SECRET
+    )
+    res.json({token})
+  }else{
+    res.status(401).json({error: "No sir/ma'm"})
+  }
 })
 
 const PORT = process.env.PORT || 5000
